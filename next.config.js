746000a652fc828a80fa6d5603cb4fdbaa1fdf35
kosaku kurino <kousaku.maron@gmail.com>
2020-08-05 09:52:39 +0900

    fix: build env setting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 const path = require('path')
 
 module.exports = {
+  build: {
+    env: {
+      API_KEY: process.env.API_KEY,
+      AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+      DATABASE_URL: process.env.DATABASE_URL,
+      PROJECT_ID: process.env.PROJECT_ID,
+      STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+      MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID
+    }
+  },
   env: {
     API_KEY: process.env.API_KEY,
     AUTH_DOMAIN: process.env.AUTH_DOMAIN,
@@ -22,4 +32,5 @@ module.exports = {
 
     return config
   }
+
 }
